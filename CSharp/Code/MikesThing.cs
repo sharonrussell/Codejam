20a8987e2f4421237e6d02877f04a3249a4d144b
Mike Parker <michael.parker@comparethemarket.com>
2016-03-15 15:41:21 +0000

    First passing test from getting some stuff from the rest api

---
+++ CSharp/Code/MikesThing.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using RestSharp;
+
+namespace Code
+{
+    public class MikesThing
+    {
+        private readonly RestClient client = new RestClient("http://swapi.co/api/");
+
+        public List<dynamic> GetFilms()
+        {
+            RestRequest request = new RestRequest("films", Method.GET);
+            IRestResponse<List<dynamic>> response = client.Execute<List<dynamic>>(request);
+            return response.Data;
+        }
+    }
+}
---
+++ CSharp/Tests/StarWarsAPITests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Code;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class StarWarsAPITests
+    {
+        [Test]
+        public void TestThat_WeCanReceiveSomeOutput()
+        {
+            var sut = new MikesThing();
+
+            var output = sut.GetFilms();
+
+            Assert.NotNull(output);
+        }
+    }
+}
