fa8126f47fac5f4e299d4440ef06b95e29f19298
pavlosmcg <paul.mcgrath@comparethemarket.com>
2016-03-15 15:34:22 +0000

    make first test pass

--- CSharp/Code/NameTranslator.cs
+++ CSharp/Code/NameTranslator.cs
@@ -8,5 +8,20 @@ namespace Code
 {
     public class NameTranslator
     {
+        public string Translate(string character)
+        {
+            var names = new Dictionary<string, string>
+            {
+                {"C-3P0", "Sergei" },
+                {"R2-D2", "Oleg" },
+                {"Darth Vader", "Vassilli" },
+                {"Leia", "Maiya" },
+                {"Luke Skywalker", "Bogdan" },
+                {"Obi-wan", "Alexander" },
+                {"Han Solo", "Yakov" },
+            };
+
+            return names[character];
+        }
     }
 }
--- CSharp/Tests/Tests.cs
+++ CSharp/Tests/Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Code;
+using NUnit.Framework;
 
 namespace Tests
 {
