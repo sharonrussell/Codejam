39b72dd5f334c87ac66d84df185a566d7f167604
pavlosmcg <paul.mcgrath@comparethemarket.com>
2016-03-15 15:26:13 +0000

    first test of name translator

---
+++ CSharp/Tests/NameTranslatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Code;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    class NameTranslatorTests
+    {
+        [Test]
+        public void Test_TranslatesNames()
+        {
+            var unitundertest = new NameTranslator();
+            var character = "Luke Skywalker";
+
+            var result = unitundertest.Translate(character);
+
+            Assert.That(result, Is.EqualTo("Bogdan"));
+        }
+    }
+}
