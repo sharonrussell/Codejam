dab3bf7d95006f3f939f9c1adf0dab3b33da2235
Mike Parker <michael.parker@comparethemarket.com>
2016-03-15 15:19:16 +0000

    First ocmmit

--- CSharp/Tests/Tests.cs
+++ CSharp/Tests/Tests.cs
@@ -8,7 +8,12 @@ namespace Tests
         [Test]
         public void FirstTest()
         {
-            
+            // ideas for first test:
+            // Talk to api?
+            // translate meerkat names
+            // strip out character names out of text
+            // given JSON, extract meerkat names
+            var sut = new MikesThing();
         }
     }
 }
